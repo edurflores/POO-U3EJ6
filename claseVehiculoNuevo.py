2fd7a5834a7301be73448959a8d6b622cf3e2018
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2021-06-17 15:30:00 -0300

    Update claseVehiculoNuevo.py

--- claseVehiculoNuevo.py
+++ claseVehiculoNuevo.py
@@ -4,8 +4,8 @@ class VehiculoNuevo(Vehiculo):
     __marca = 'Lamborghini'
     __version = '' # Base o Full
 
-    def __init__(self,mod,cpuer,color,precio,ver,marc,pat,an,km):
-        super().__init__(mod,cpuer,color,precio,ver,marc,pat,an,km)
+    def __init__(self,mod,cpuer,color,precio,ver):
+        super().__init__(mod,cpuer,color,precio)
         self.__version = ver
 
     def getMarca(self): # Ojo posible metodo estatico
@@ -13,3 +13,21 @@ class VehiculoNuevo(Vehiculo):
 
     def getVersion(self):
         return self.__version
+
+    def CalculaImporteVenta(self):
+        patentamiento = ((10 * super().getPrecioBase()) / 100) # Gastos de patentamiento
+        costofull = ((2 * super().getPrecioBase()) / 100) # 2% si es full
+        if self.getVersion() == 'Full':
+            importe = super().getPrecioBase() + patentamiento + costofull
+        else:
+            importe = super().getPrecioBase() + patentamiento
+        return importe
+
+    def MuestraDatos(self):
+        print('Marca:', self.__marca)
+        print('Modelo:', super().getModelo())
+        print('Version:',self.__version)
+        print('Cantidad de puertas:', super().getCantidadPuertas())
+        print('Color:', super().getColor())
+        print('Precio base:', super().getPrecioBase())
+        print('Importe final de venta:', self.CalculaImporteVenta())
