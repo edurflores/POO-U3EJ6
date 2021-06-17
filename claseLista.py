5d5ea3257d96f7ad2ff6a1c65a60579a7a6b8866
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2021-06-17 15:31:09 -0300

    Update claseLista.py

--- claseLista.py
+++ claseLista.py
@@ -1,4 +1,6 @@
 from classNodo import Nodo
+from claseVehiculoUsado import VehiculoUsado
+from claseVehiculoNuevo import VehiculoNuevo
 
 class Lista:
     __comienzo = None
@@ -27,3 +29,70 @@ class Lista:
         self.__comienzo = nodo
         self.__actual = nodo
         self.__tope += 1
+
+    def CreaVehiculo(self):
+        print('Carga de vehiculo.')
+        mod = input('Ingrese modelo:')
+        cpuer = int(input('Ingrese cantidad de puertas:'))
+        color = input('Ingrese color:')
+        precio = float(input('Ingrese precio base:'))
+        op = int(input('¿Es un vehiculo nuevo? 1- Si. 2- No.'))
+        if op == 1:
+            ver = input('Ingrese version: (Base/Full):')
+            unVehiculo = VehiculoNuevo(mod,cpuer,color,precio,ver)
+            self.AgregarVehiculo(unVehiculo)
+            print('Se ha registrado el vehiculo.')
+        elif op == 2:
+            marc = input('Ingrese marca:')
+            pat = input('Ingrese patente:')
+            an = int(input('Ingrese anho:'))
+            km: float = float(input('Ingrese kilometraje:'))
+            unVehiculo = VehiculoUsado(mod,cpuer,color,precio,marc,pat,an,km)
+            self.AgregarVehiculo(unVehiculo)
+            print('Se ha registrado el vehiculo.')
+
+    def ModPrecioVehiculoUsado(self,pat): # Consigna 4
+        for vehiculo in self:
+            ban = False
+            if isinstance(vehiculo,VehiculoUsado):
+                if vehiculo.getPatente() == pat:
+                    ban = True
+                    print('Modificar precio de vehiculo usado.')
+                    nuevopre = float(input('Ingrese nuevo precio base:'))
+                    vehiculo.setPrecioBase(nuevopre)
+                    print('Nuevo importe de venta:',vehiculo.CalculaImporteVenta())
+                    print('------------------------------------')
+                    break
+        if ban == False:
+            print('Error. No se encontro vehiculo usado con la patente proporcionada.')
+            print('------------------------------------')
+
+    def DatosMasEconomico(self): # Consigna 5 (Economico segun el precio base)
+        print('Datos del vehiculo mas economico.')
+        minprecio = self.__comienzo.getVehiculo().getPrecioBase() # Precio minimo registrado, lo inicializa con el primero
+        vehiculominimo = self.__comienzo.getVehiculo()
+        for vehiculo in self:
+            if vehiculo.getPrecioBase() < minprecio:
+                minprecio = vehiculo.getPrecioBase()
+                vehiculominimo = vehiculo
+        vehiculominimo.MuestraDatosBasicos()
+        if isinstance(vehiculominimo,VehiculoUsado):
+            print('Tipo de vehiculo: Usado.')
+            vehiculominimo.MuestraDatos()
+        elif isinstance(vehiculominimo,VehiculoNuevo):
+            print('Tipo de vehiculo: Nuevo')
+            vehiculominimo.MuestraDatos()
+
+    def MuestraTodos(self): # Consigna 6
+        print('Vehiculos a la venta.')
+        print('------------------------------------')
+        for vehiculo in self:
+            if isinstance(vehiculo,VehiculoNuevo):
+                print('Tipo de vehiculo: Nuevo')
+            else:
+                print('Tipo de vehiculo: Usado')
+            print('Modelo:',vehiculo.getModelo())
+            print('Cantidad de puertas:',vehiculo.getCantidadPuertas())
+            print('Importe de venta:',vehiculo.CalculaImporteVenta())
+            print('------------------------------------')
+
