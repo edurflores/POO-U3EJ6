a2a13712b9adb587dd72174245d21d599e298c89
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2021-06-17 15:29:39 -0300

    Update claseVehiculoUsado.py

--- claseVehiculoUsado.py
+++ claseVehiculoUsado.py
@@ -6,8 +6,8 @@ class VehiculoUsado(Vehiculo):
     __anho = 0
     __kilometraje = 0.0
 
-    def __init__(self,mod,cpuer,color,precio,ver,marc,pat,an,km):
-        super().__init__(mod,cpuer,color,precio,ver,marc,pat,an,km)
+    def __init__(self,mod,cpuer,color,precio,marc,pat,an,km):
+        super().__init__(mod,cpuer,color,precio)
         self.__marca = marc
         self.__patente = pat
         self.__anho = an
@@ -19,3 +19,17 @@ class VehiculoUsado(Vehiculo):
         return self.__anho
     def getKilometraje(self):
         return self.__kilometraje
+    def CalculaImporteVenta(self):
+        antiguedad = 2021 - self.__anho
+        importe = super().getPrecioBase() - (((super().getPrecioBase() * 1) / 100) * antiguedad)
+        return importe
+    def MuestraDatos(self):
+        print('Marca:',self.__marca)
+        print('Modelo:',super().getModelo())
+        print('Anho:',self.__anho)
+        print('Cantidad de puertas:',super().getCantidadPuertas())
+        print('Color:',super().getColor())
+        print('Patente:',self.__patente)
+        print('Kilometraje:',self.__kilometraje)
+        print('Precio base:',super().getPrecioBase())
+        print('Importe final de venta:',self.CalculaImporteVenta())
